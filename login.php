66b01f53b70e504cc5a1fb0d02ae328679bc9928
AKHID\Reksaning <reksa.akhid@gmail.com>
2017-06-16 14:36:57 +0700

    first init

---
+++ login.php
@@ -0,0 +1,16 @@
+
+
+<form role="form" method="post" action="ceklog.php">
+	<fieldset>
+		<div class="form-group">
+<input class="form-control" placeholder="Username"
+name="username" autofocus>
+		</div>
+		<div class="form-group">
+<input class="form-control" placeholder="Password"
+name="password" type="password value="">
+		</div>
+
+<button class="btn btn-lg btn-success btn-block">Login</a>
+</fieldset>
+</form>
